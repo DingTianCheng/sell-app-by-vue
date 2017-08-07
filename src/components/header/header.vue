55e353e840a891e0d799d258c565ad01cd8eb3a4
DingTianCheng <17051037502@sina.cn>
2017-08-07 14:44:38 +0800

    头部组件和star组件开发完成

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -35,10 +35,36 @@
 			 <img :src="seller.avatar" alt="" width="100%" height="auto" />
 		</div>
 		<!-- 详情弹出层 -->
-		<div class="detail" v-show="detailShow">
+		<div class="detail" v-show="detailShow" transition="fade">
 			<div class="detail-wapper clearfix">
 				<div class="detail-main">
 					<h1 class="detail-main-title">{{seller.name}}</h1>
+					<div class="star-wapper">
+						<star :size="48" :score="seller.score"></star>
+					</div>
+					<div class="detail-title">
+						<div class="detail-title-line"></div>
+						<div class="detail-title-text">优惠信息</div>
+						<div class="detail-title-line"></div>
+					</div>
+					<div class="detail-msg-wapper">
+						<ul v-if="seller.supports" class="detail-msg">
+							<li class="detail-msg-item" v-for="item in seller.supports">
+								<span class="detail-msg-item-icon" :class="classMap[seller.supports[$index].type]"></span>
+								<span class="detail-msg-item-text">{{seller.supports[$index].description}}</span>
+							</li>
+						</ul>
+					</div>
+					<div class="detail-title">
+						<div class="detail-title-line"></div>
+						<div class="detail-title-text">商家公告</div>
+						<div class="detail-title-line"></div>
+					</div>
+					<div class="detail-bulletin-wapper">
+						<p class="detail-bulletin">
+							{{seller.bulletin}}
+						</p>
+					</div>
 				</div>
 			</div>
 			<div class="detail-close">
@@ -49,6 +75,7 @@
 </template>
 
 <script>
+import star from 'components/star/star.vue';
 export default {
 	props: [ 'seller' ],
 	data () {
@@ -64,8 +91,11 @@ export default {
 			this.detailShow = false;
 		}
 	},
+	components: {
+		star
+	},
 	created () {
-	this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+		this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
 	}
 };
 </script>
@@ -125,23 +155,23 @@ export default {
 		 	width: 12px;
 		 	height: 12px;
 		 }
-		 .icon.decrease{
+		 .decrease{
 		 	background: url(decrease_1@2x.png) 0px 0px no-repeat;
 		 	background-size: 12px 12px;
 		 }
-		 .icon.discount{
+		 .discount{
 		 	background: url(discount_1@2x.png) 0px 0px no-repeat;
 		 	background-size: 12px 12px;
 		 }
-		 .icon.guarantee{
+		 .guarantee{
 		 	background: url(guarantee_1@2x.png) 0px 0px no-repeat;
 		 	background-size: 12px 12px;
 		 }
-		 .icon.invoice{
+		 .invoice{
 		 	background: url(invoice_1@2x.png) 0px 0px no-repeat;
 		 	background-size: 12px 12px;
 		 }
-		 .icon.special{
+		 .special{
 		 	background: url(special_1@2x.png) 0px 0px no-repeat;
 		 	background-size: 12px 12px;
 		 }
@@ -212,41 +242,101 @@ export default {
 		}
 
 	/* 详情弹出层 */
-	.detail{
-		position: fixed;
-		left:0;
-		top:0;
-		z-index: 100;
-		width: 100%;
-		height: 100%;
-		background: rgba(7,17,27,0.8);
-		overflow: auto;
-	}
-	.detail-wapper{
-		min-height: 100%;
-		overflow: hidden;
-	}
-	.detail-wapper .detail-main{
-		margin-top:64px;
-		padding-bottom: 96px;
-	}
-	.detail-main-title{
-		margin-bottom: 16px;
-		line-height: 16px;
-		font-size: 16px;
-		font-weight: 700;
-		text-align: center;
-	}
-	.detail-close{
-		height: 32px;
-		line-height: 32px;
-		margin-top: -64px;
-		text-align: center;
-	}
-	.detail-close i{
-		color: #fff;
-		font-size: 32px;
-	}
+		.detail{
+			position: fixed;
+			left:0;
+			top:0;
+			z-index: 100;
+			width: 100%;
+			height: 100%;
+			overflow: auto;
+			transition: all 0.5s ease;
+			backdrop-filter:blur(10px)
+		}
+		.fade-transition{
+			opacity: 1;
+			background: rgba(7,17,27,0.8);
+		}
+		.fade-enter,.fade-leave{
+			opacity: 0;
+			background: rgba(7,17,27,0);
+		}
+		.detail-wapper{
+			min-height: 100%;
+			overflow: hidden;
+		}
+		.detail-wapper .detail-main{
+			margin-top:64px;
+			padding-bottom: 96px;
+		}
+		.detail-main-title{
+			margin-bottom: 16px;
+			line-height: 16px;
+			font-size: 16px;
+			font-weight: 700;
+			text-align: center;
+		}
+		.star-wapper{
+			text-align: center;
+			margin-bottom: 28px;
+		}
+		.detail-main .detail-title{
+			display: flex;
+			width:80%;
+			margin:28px auto 24px auto;
+		}
+		.detail-main .detail-title-line{
+			flex: 1;
+			position: relative;
+			top:-6px;
+			border-bottom: 1px solid rgba(255,255,255,0.2)
+		}
+		.detail-main .detail-title-text{
+			padding:0 12px;
+			font-size: 14px;
+		}
+		.detail-bulletin-wapper{
+			width: 80%;
+			margin: 0 auto;
+		}
+		.detail-bulletin{
+			padding:0 12px;
+			font-size: 12px;
+			line-height: 24px;
+		}
+		.detail-msg-wapper{
+			width: 80%;
+			margin:0 auto;
+		}
+		.detail-msg{
+			padding: 0 12px;
+		}
+		.detail-msg-item{
+			margin-bottom: 12px;
+			font-size: 0;
+		}
+		.detail-msg-item-icon{
+			display: inline-block;
+			vertical-align: top;
+			margin-right: 6px;
+			width: 12px;
+			height: 12px;
+		}
+
+		.detail-msg-item-text{
+			line-height: 12px;
+			font-size: 12px;
+		}
+		.detail-close{
+			height: 32px;
+			line-height: 32px;
+			margin-top: -64px;
+			text-align: center;
+		}
+		.detail-close i{
+			color: #fff;
+			font-size: 24px;
+		}
 
 	@media(min-device-pixel-ratio:1.5),(-webkit-min-device-pixel-ratio:1.5){
 			 .title .brand{
--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -1,14 +1,72 @@
 <template>
 	<div class="star" :class="starType">
-		<span></span>
+		<span v-for="itemCalss in itemClasses" track-by="$index" :class="itemCalss" class="star-item"></span>
 	</div>
 </template>
 
 <script>
-export default {
-};
+	const LENGTH = 5;
+	const CLS_ON = 'on';
+	const CLS_HALF = 'half';
+	const CLS_OFF = 'off';
+	export default {
+		props: {
+			size: {
+				type: Number
+			},
+			score: {
+				type: Number
+			}
+		},
+		computed: {
+			starType () {
+				return 'star-' + this.size;
+			},
+			itemClasses () {
+				let result = [];
+				// 把得来的分数整理，分为 x 或 x.5
+				let score = Math.floor(this.score * 2) / 2;
+				// 判断是否有余数0.5
+				let hasDecimal = score % 1 !== 0;
+				// 取整数部分
+				let integer = Math.floor(score);
+				for (let i = 0; i < integer; i++) {
+					result.push(CLS_ON);
+				}
+				if (hasDecimal) {
+					result.push(CLS_HALF);
+				}
+				while (result.length < LENGTH) {
+					result.push(CLS_OFF);
+				}
+				return result;
+			}
+		}
+	};
 </script>
 
 <style scoped>
-	.star{  }
+	div.star{ font-size: 0; }
+	div.star span.star-item{ display: inline-block; }
+
+	div.star-48{  }	
+	div.star-48 span.star-item{ width: 20px;height: 20px;margin-right: 22px; }
+	div.star-48 span.star-item:nth-last-child(1){ margin-right: 0px; }
+	div.star-48 span.on{ background: url(images/star48_on@2x.png) 0px 0px no-repeat;background-size:20px 20px; }	
+	div.star-48 span.half{ background: url(images/star48_half@2x.png) 0px 0px no-repeat;background-size:20px 20px; }	
+	div.star-48 span.off{ background: url(images/star48_off@2x.png) 0px 0px no-repeat;background-size:20px 20px; }
+
+	div.star-36{  }	
+	div.star-36 span.star-item{ width: 15px;height: 15px;margin-right: 16px;background-size: 15px 15px; }
+	div.star-36 span.star-item:nth-last-child(1){ margin-right: 0px; }
+	div.star-36 span.on{ background: url(images/star36_on@2x.png) 0px 0px no-repeat;background-size:15px 15px; }	
+	div.star-36 span.half{ background: url(images/star36_half@2x.png) 0px 0px no-repeat;background-size:15px 15px; }	
+	div.star-36 span.off{ background: url(images/star36_off@2x.png) 0px 0px no-repeat;background-size:15px 15px; }
+
+	div.star-24{  }
+	div.star-24 span.star-item{ width: 10px;height: 10px;margin-right: 3px; }
+	div.star-24 span.star-item:nth-last-child(1){ margin-right: 0px; }
+	div.star-24 span.on{ background: url(images/star24_on@2x.png) 0px 0px no-repeat;background-size: 10px 10px; }	
+	div.star-24 span.half{ background: url(images/star24_half@2x.png) 0px 0px no-repeat;background-size: 10px 10px; }	
+	div.star-24 span.off{ background: url(images/star24_off@2x.png) 0px 0px no-repeat;background-size: 10px 10px; }
 </style>
